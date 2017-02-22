57424ea02e6fc4a01d01a06505181c9723e8328f
Irina Toroshchina <nemnogoira@yandex.ru>
2017-02-23 00:20:27 +0500

    static comp ffmpeg + test it

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -5,13 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 using Exporter;
+using Exporter.Video;
 
 namespace Demo
 {
     class Program
     {
         static void Main(string[] args) {
-
+            MP4.OpenMW("C:\\Users\\Home\\Source\\Repos\\DrawMoar\\Demo\\bin\\Debug\\ffmpeg.exe");
+            
         }
     }
 }
--- Exporter/Video/MP4.cs
+++ Exporter/Video/MP4.cs
@@ -5,11 +5,18 @@ using System.Text;
 using System.Threading.Tasks;
 
 using BaseElements;
+using System.Diagnostics;
 
 namespace Exporter.Video
 {
     public class MP4 : IVideoExporter
     {
+        public static void OpenMW(string file) {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = file;
+            Process.Start(startInfo);
+        }
+
         public void Save(Cartoon cartoon, string path) {
 
         }
