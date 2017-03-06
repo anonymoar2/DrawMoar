b970d350c70bd2daf515ab2ad03d0eafa22ea00d
Irina Toroshchina <nemnogoira@yandex.ru>
2017-03-06 15:20:17 +0500

    rename WPFGUI to DrawMoar

--- DrawMoar/App.xaml.cs
+++ DrawMoar/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace WPFGUI
+namespace DrawMoar
 {
     /// <summary>
     /// Логика взаимодействия для App.xaml
--- DrawMoar/MainWindow.xaml.cs
+++ DrawMoar/MainWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFGUI
+namespace DrawMoar
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
