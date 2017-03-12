6f0f8256c1442de65f5c56c0af78e64daf5d1dbd
Irina Toroshchina <nemnogoira@yandex.ru>
2017-03-13 01:46:53 +0500

    change structure of Model of GrEditor

--- BaseElements/RasterLayer.cs
+++ BaseElements/RasterLayer.cs
@@ -5,15 +5,55 @@ using System.Text;
 using System.Drawing;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace BaseElements
 {
-    internal class RasterLayer : Layer
+    /// TODO: Реализовать растровый слой
+    public class RasterLayer : ILayer
     {
-        /// TODO: Реализовать растровый слой
+        /// <summary>
+        /// Название (имя) слоя
+        /// </summary>
+        private string name;
+        public string Name {
+            get { return name; }
+            set {
+                // TODO: Изменить регулярное выражение на более подходящее
+                if (Regex.IsMatch(value, @"[a-zA-Z0-9]+")) {
+                    name = value;
+                }
+                else {
+                    throw new ArgumentException("Название слоя должно состоять только " +
+                                                "из латинских букв и цифр.");
+                }
+            }
+        }
 
-        
-        public override void Save() {
+
+        /// <summary>
+        /// True - слой видимый, false - невидимый
+        /// </summary>
+        private bool visible = true;
+        public bool Visible {
+            get {
+                return visible;
+            }
+
+            set {
+                visible = value;
+            }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public RasterLayer() {
+
+        }
+
+        public virtual void Draw() {
             throw new NotImplementedException();
         }
     }
--- BaseElements/VectorLayer.cs
+++ BaseElements/VectorLayer.cs
@@ -5,12 +5,51 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 
 using BaseElements.Figures;
+using System.Text.RegularExpressions;
 
 namespace BaseElements
 {
-    internal class VectorLayer: Layer
+    public class VectorLayer : ILayer
     {
-        public override void Save() {
+        /// <summary>
+        /// Название (имя) слоя
+        /// </summary>
+        private string name;
+        public string Name {
+            get { return name; }
+            set {
+                // TODO: Изменить регулярное выражение на более подходящее
+                if (Regex.IsMatch(value, @"[a-zA-Z0-9]+")) {
+                    name = value;
+                }
+                else {
+                    throw new ArgumentException("Название слоя должно состоять только " +
+                                                "из латинских букв и цифр.");
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// True - слой видимый, false - невидимый
+        /// </summary>
+        private bool visible = true;
+        public bool Visible {
+            get {
+                return visible;
+            }
+
+            set {
+                visible = value;
+            }
+        }
+
+
+        public VectorLayer() {
+
+        }
+
+        public virtual void Draw() {
             throw new NotImplementedException();
         }
     }
