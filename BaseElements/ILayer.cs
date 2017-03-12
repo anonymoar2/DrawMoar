9bd774bdd377e7ac2db03784570eef23bac7a86c
Irina Toroshchina <nemnogoira@yandex.ru>
2017-03-13 02:59:30 +0500

    add ILayer

---
+++ BaseElements/ILayer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseElements
+{
+    interface ILayer
+    {
+        /// <summary>
+        /// Состояние видимости слоя. 
+        /// true - слой видимый, false - невидимый.
+        /// При экспорте кадра в картинку картинка получается только из видимых слоёв.
+        /// </summary>
+        bool Visible { get; set; }
+
+        /// <summary>
+        /// Название слоя.
+        /// </summary>
+        string Name { get; set; }
+
+        void Draw();
+    }
+}
