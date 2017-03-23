2af092d875285b4a3dec797ee2009ea7a949888b
Irina Toroshchina <nemnogoira@yandex.ru>
2017-03-23 12:16:32 +0500

    Написала Rotate для точки

--- DrawMoar/BaseElements/RotateTransformation.cs
+++ DrawMoar/BaseElements/RotateTransformation.cs
@@ -16,14 +16,18 @@ namespace DrawMoar.BaseElements
         private Matrix<double> Transform;
 
         public RotateTransformation(System.Windows.Point point, double angle) {
-
+            var one = new Matrix<double>(new double[3, 3] { { 1, 0, point.X }, { 0, 1, point.Y }, { 0, 0, 1 } });
+            var three = new Matrix<double>(new double[3, 3] { { 1, 0, -point.X }, { 0, 1, -point.Y }, { 0, 0, 1 } });
+            var two = new Matrix<double>(new double[3, 3] {{ Math.Cos(angle * (Math.PI / 180)), -Math.Sin(angle * (Math.PI / 180)), 0 }, { Math.Sin(angle * (Math.PI / 180)),  Math.Cos(angle * (Math.PI / 180)), 0 }, {0,0,1 }});
+            Transform = one * two * three;
         }
 
         public override System.Windows.Point Apply(System.Windows.Point point) {
-            throw new NotImplementedException();
+            var rP = Transform * new Matrix<double>(new double[3, 1] { { point.X }, { point.Y }, { 1 } });
+            return new System.Windows.Point(rP[0, 0], rP[1, 0]);
         }
 
-        public override Image Apply(Image image) {
+        public override Picture Apply(Picture picture) {
             throw new NotImplementedException();
         }
     }
--- DrawMoar/BaseElements/Transformation.cs
+++ DrawMoar/BaseElements/Transformation.cs
@@ -11,6 +11,11 @@ namespace DrawMoar.BaseElements
     /// </summary>
     public abstract class Transformation
     {
+        /// <summary>
+        /// Думаю можно не возвращать тут ничего, менять содержимое, подумать над этим (!)
+        /// </summary>
+        /// <param name="picture"></param>
+        /// <returns></returns>
         public abstract Picture Apply(Picture picture);
         public abstract System.Windows.Point Apply(System.Windows.Point point);
     }
