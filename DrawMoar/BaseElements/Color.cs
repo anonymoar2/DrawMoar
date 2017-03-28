c4a5b5afc01030040aac4d9374fa06ff5dc906b9
Antoha1397 <toscha.17@mail.ru>
2017-03-28 16:13:51 +0500

    Color

---
+++ DrawMoar/BaseElements/Color.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace DrawMoar
+{
+    class Color
+    {
+        int R;
+        int G;
+        int B;
+
+        public Color()
+        {
+            R = 0;
+            G = 0;
+            B = 0;
+        }
+
+        public Color(System.Windows.Media.Color color)
+        {
+            this.R = color.R;
+            this.G = color.G;
+            this.B = color.B;
+        }
+
+        public System.Windows.Media.Color Convert(System.Drawing.Color color)
+        {              
+            System.Windows.Media.Color newColor = System.Windows.Media.Color.FromArgb(color.A, color.R, color.G, color.B);
+            return newColor;
+        }
+     
+        public System.Drawing.Color Convert(System.Windows.Media.Color color)
+        {
+            System.Drawing.Color newColor = System.Drawing.Color.FromArgb(color.A, color.R, color.G, color.B);
+            return newColor;
+        }     
+    }
+}
